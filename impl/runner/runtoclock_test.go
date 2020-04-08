7b30e170d9e51f9e0bb9f5b0f32796a5dfa998ba
Srini <s@srin.me>
2020-04-08 16:05:45 -0400

    add channel for the tests

--- impl/runner/runtoclock_test.go
+++ impl/runner/runtoclock_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestRunToClock(t *testing.T) {
-
+	comm := make(chan string)
 	args := []string{"ls"}
 	clockspec, _ := time.Parse("15:04:05", "01:00:20")
-	RunToClock(clockspec, false, args)
+	RunToClock(clockspec, false, args, comm)
 	clockspec2, _ := time.Parse("15:04:05", "01:30:20")
-	RunToClock(clockspec2, false, args)
+	RunToClock(clockspec2, false, args, comm)
 	clockspec3, _ := time.Parse("15:04:05", "01:15:00")
-	RunToClock(clockspec3, false, args)
+	RunToClock(clockspec3, false, args, comm)
 
 }
